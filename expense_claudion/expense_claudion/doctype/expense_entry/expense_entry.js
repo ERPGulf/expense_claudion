183a03e9c17a0381d5372833c80926baae9261f9
Harshafiroz <harsha@htsqatar.com>
2026-06-09 12:11:46 +0300

    gl entry

--- expense_claudion/expense_claudion/doctype/expense_entry/expense_entry.js
+++ expense_claudion/expense_claudion/doctype/expense_entry/expense_entry.js
@@ -1,18 +1,12 @@
 frappe.ui.form.on('Expense Entry', {
     setup(frm) {
-        frm.set_query('claimant_type', () => {
-            return {
-                query: 'expense_claudion.api.get_party_doctypes'
-            };
-        });
+        frm.set_query('claimant_type', () => ({
+            query: 'expense_claudion.api.get_party_doctypes'
+        }));
 
-        frm.set_query('tax_account', () => {
-            return {
-                filters: {
-                    account_type: 'Tax'
-                }
-            };
-        });
+        frm.set_query('tax_account', () => ({
+            filters: { account_type: 'Tax', is_group: 0}
+        }));
     },
 
     onload(frm) {
@@ -29,30 +23,57 @@ frappe.ui.form.on('Expense Entry', {
         frm.set_value('payable_account', null);
 
         frm.set_query('payable_account', () => {
-            let filters = {};
+            let filters = {is_group: 0};
 
             if (frm.doc.payment_method === 'Reimburse Later') {
                 filters.account_type = 'Payable';
-
             } else if (
                 frm.doc.payment_method === 'Cash' ||
                 frm.doc.payment_method === 'Petty Cash'
             ) {
                 filters.account_type = 'Cash';
-
             } else if (frm.doc.payment_method === 'Bank Transfer') {
                 filters.account_type = 'Bank';
             }
 
             return { filters };
         });
+    },
+    refresh(frm) {
+    if (frm.is_new()) return;
+
+    if (frm.doc.docstatus === 0) {
+        frm.add_custom_button(__('Ledger Preview'), () => {
+            show_ledger_preview(frm);
+        }, __('Preview'));
     }
+
+    if (frm.doc.docstatus === 1) {
+        frm.add_custom_button(__('General Ledger'), () => {
+            frappe.set_route('query-report', 'General Ledger', {
+                voucher_type: frm.doc.doctype,
+                voucher_no: frm.doc.name
+            });
+        }, __('View'));
+    }
+}
+
 });
+
+
 
 
 frappe.ui.form.on('Expense Lines', {
     tax_template(frm, cdt, cdn) {
         const row = locals[cdt][cdn];
+
+        if (row.tax_template && !frm.doc.payable_account) {
+            frappe.msgprint({
+                title: __('Payable Account Required'),
+                message: __('Please select a Payable Account before applying tax.'),
+                indicator: 'orange'
+            });
+        }
 
         if (!row.tax_template) {
             row.tax_ = 0;
@@ -62,16 +83,13 @@ frappe.ui.form.on('Expense Lines', {
             return;
         }
 
-        frappe.db.get_value(
-            'Tax Template',
-            row.tax_template,
-            'percentage_'
-        ).then(r => {
-            row.tax_ = r.message?.percentage_ || 0;
-            calculate_row_total(row);
-            update_summary(frm);
-            frm.refresh_field('expense_lines');
-        });
+        frappe.db.get_value('Tax Template', row.tax_template, 'percentage_')
+            .then(r => {
+                row.tax_ = r.message?.percentage_ || 0;
+                calculate_row_total(row);
+                update_summary(frm);
+                frm.refresh_field('expense_lines');
+            });
     },
 
     amount(frm, cdt, cdn) {
@@ -93,14 +111,59 @@ frappe.ui.form.on('Expense Lines', {
 
 
 
+
+function show_ledger_preview(frm) {
+    frappe.call({
+        method: 'get_gl_preview',
+        doc: frm.doc,
+        callback(r) {
+            if (r.message) {
+                const d = new frappe.ui.Dialog({
+                    title: __('Accounting Ledger Preview'),
+                    size: 'large',
+                    fields: [{ fieldtype: 'HTML', fieldname: 'html' }]
+                });
+
+                d.fields_dict.html.$wrapper.html(render_gl_table(r.message));
+                d.show();
+            }
+        }
+    });
+}
+
+function render_gl_table(entries) {
+    let rows = entries.map(e => `
+        <tr>
+            <td>${e.account}</td>
+            <td class="text-right">${format_currency(e.debit)}</td>
+            <td class="text-right">${format_currency(e.credit)}</td>
+            <td>${e.remarks || ''}</td>
+        </tr>
+    `).join('');
+
+    return `
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Account</th>
+                    <th>Debit</th>
+                    <th>Credit</th>
+                    <th>Remarks</th>
+                </tr>
+            </thead>
+            <tbody>${rows}</tbody>
+        </table>
+    `;
+}
+
+
+
 function calculate_row_total(row) {
     const amount = flt(row.amount);
     const tax_percent = flt(row.tax_);
-
     row.tax_amount = flt(amount * tax_percent / 100);
     row.total = flt(amount + row.tax_amount);
 }
-
 
 function update_summary(frm) {
     let expense_subtotal = 0;
@@ -112,10 +175,7 @@ function update_summary(frm) {
         expense_subtotal += flt(row.amount);
         total_vat += flt(row.tax_amount);
         grand_total += flt(row.total);
-
-        if (flt(row.tax_) > 0) {
-            taxed_lines += 1;
-        }
+        if (flt(row.tax_) > 0) taxed_lines++;
     });
 
     frm.set_value('expense_subtotal', expense_subtotal);
--- expense_claudion/expense_claudion/doctype/expense_entry/expense_entry.py
+++ expense_claudion/expense_claudion/doctype/expense_entry/expense_entry.py
@@ -1,9 +1,118 @@
-# Copyright (c) 2026, erpgulf.com and contributors
-# For license information, please see license.txt
+# Copyright (c) 2026, erpgulf.com
+# License: see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
+from erpnext.accounts.general_ledger import make_gl_entries
+from erpnext.accounts.utils import get_account_currency
 
 
 class ExpenseEntry(Document):
-	pass
+
+
+    def on_submit(self):
+        self.make_gl_entries()
+
+    def on_cancel(self):
+        frappe.db.sql(
+            """
+            DELETE FROM `tabGL Entry`
+            WHERE voucher_type=%s AND voucher_no=%s
+            """,
+            (self.doctype, self.name),
+        )
+
+   
+    @frappe.whitelist()
+    def get_gl_preview(self):
+        return self.build_gl_entries(preview=True)
+
+
+    def make_gl_entries(self):
+        gl_entries = self.build_gl_entries(preview=False)
+        make_gl_entries(gl_entries, cancel=False, adv_adj=False)
+
+
+    def build_gl_entries(self, preview=False):
+        gl_entries = []
+        total_tax = 0
+
+        for row in self.expense_lines:
+            gl_entries.append(
+                self.get_gl_dict(
+                    account=row.account,
+                    debit=row.amount,
+                    credit=0,
+                    remarks=row.description or self.remarks or "Expense",
+                )
+            )
+            total_tax += flt(row.tax_amount)
+
+        if total_tax > 0 and self.tax_account:
+            gl_entries.append(
+                self.get_gl_dict(
+                    account=self.tax_account,
+                    debit=total_tax,
+                    credit=0,
+                    remarks="Tax",
+                )
+            )
+
+        gl_entries.append(
+            self.get_gl_dict(
+                account=self.payable_account,
+                debit=0,
+                credit=self.grand_total,
+                remarks="Expense Settlement",
+            )
+        )
+
+        return gl_entries
+
+
+    def get_gl_dict(self, account, debit=0, credit=0, remarks=None):
+        """
+        Correct GL Entry for ERPNext v15
+        """
+
+        account_type, is_group = frappe.db.get_value(
+            "Account", account, ["account_type", "is_group"]
+        )
+
+        if is_group:
+            frappe.throw(f"Account {account} is a Group Account")
+
+        account_currency = get_account_currency(account)
+
+        party_type = None
+        party = None
+
+        if account_type in ("Payable", "Receivable"):
+            party_type = self.claimant_type
+            party = self.party
+
+        debit = flt(debit)
+        credit = flt(credit)
+
+        return frappe._dict({
+            "posting_date": self.posting_date,
+            "company": self.company,
+            "voucher_type": self.doctype,
+            "voucher_subtype": self.doctype,
+            "voucher_no": self.name,
+            "account": account,
+
+            "debit": debit,
+            "credit": credit,
+
+            "account_currency": account_currency,
+            "debit_in_account_currency": debit,
+            "credit_in_account_currency": credit,
+
+            "party_type": party_type,
+            "party": party,
+            "cost_center": self.cost_center,
+            "remarks": remarks,
+            "is_opening": "No",
+        })
